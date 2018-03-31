33998a02a37b79dfc2c132607e2c4b5576ed13d0
Fiqri Ismail <fiqri.ismail@gmail.com>
2018-03-31 15:51:14 +0530

    Added Chapter 9 - macOS Code

---
+++ Chapter 9/Chapter9.Xamarin/Chapter9.Xamarin.AndroidApp/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Chapter9.Xamarin.AndroidLib;
+
+namespace Chapter9.Xamarin.AndroidApp
+{
+    [Activity(Label = "Chapter9.Xamarin.AndroidApp", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 0;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            CounterLib counter = new CounterLib();
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+            button.Click += delegate { 
+                count = counter.IncrementByOne(count);
+                button.Text = $"{count} clicks!";
+            };
+        }
+    }
+}
+
---
+++ Chapter 9/Chapter9.Xamarin/Chapter9.Xamarin.AndroidLib/CounterLib.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Chapter9.Xamarin.AndroidLib
+{
+    public class CounterLib
+    {
+        public int IncrementByOne(int value)
+        {
+            value++;
+            return value;
+        }
+    }
+}
