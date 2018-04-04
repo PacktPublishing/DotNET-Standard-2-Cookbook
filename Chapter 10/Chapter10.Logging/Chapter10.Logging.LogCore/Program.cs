4035182fc4b71f6b2a70b7dad50947cea6972ce9
Fiqri Ismail <fiqri.ismail@gmail.com>
2018-04-04 10:34:49 +0530

    Chapter 10 Added

---
+++ Chapter 10/Chapter10.Logging/Chapter10.Logging.LogCore/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Chapter10.Logging.LogLib;
+
+namespace Chapter10.Logging.LogCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logDemo = new LoggerDemo();
+            Console.WriteLine("Executing method 1");
+            logDemo.CallMethod1();
+            Console.WriteLine("Executing method 2");
+            logDemo.CallMethod2();
+            Console.WriteLine("Executing method 3");
+            logDemo.CallMethod3();
+            Console.WriteLine("Press ENTER to exit.");
+            Console.ReadLine();
+
+        }
+    }
+}
