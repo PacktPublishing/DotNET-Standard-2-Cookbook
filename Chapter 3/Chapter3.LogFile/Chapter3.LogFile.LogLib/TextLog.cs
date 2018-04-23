0fadb76c7ebf43b81aa38673b6ef47d2e29756b5
Fiqri Ismail <fiqri.ismail@gmail.com>
2018-04-23 21:13:46 +0530

    Chapter 3 Updated

---
+++ Chapter 3/Chapter3.LogFile/Chapter3.LogFile.LogLib/TextLog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Chapter3.LogFile.LogLib
+{
+    public class TextLog
+    {
+        private string logFileName = "server_log.txt";
+        private StreamWriter logFile;
+
+        public TextLog()
+        {
+            if (!File.Exists(logFileName))
+            {
+                logFile = File.CreateText(logFileName);
+            }
+            else
+            {
+                logFile = File.AppendText(logFileName);
+            } 
+        }
+
+        public void WriteLog(string message)
+        {
+            logFile.WriteLine($"{DateTime.Now} Log Message: {message} ");
+        }
+
+        public void CloseLog()
+        {
+            logFile.Close();
+        }
+    }
+}
